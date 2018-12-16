63b1d801e7e13fbfd2b4a5c928b3d658ee7f7e61
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2018-12-16 16:40:33 +0100

    request

--- src/Awele/Main.java
+++ src/Awele/Main.java
@@ -1,10 +1,11 @@
 package Awele;
 
+import Awele.moteur.Game;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Bonjour !");
-        return;
+        Game game = new Game();
     }
 
 }
--- src/Awele/moteur/Game.java
+++ src/Awele/moteur/Game.java
@@ -4,6 +4,7 @@ package Awele.moteur;
 import java.util.Scanner;
 
 public class Game {
+    private static final int NB_HOLE = 12;
     private int[] blackSeeds;
     private int[] redSeeds;
     private int specialSeed;
@@ -15,16 +16,23 @@ public class Game {
 
         score1 = 0;
         score2 = 0;
+        blackSeeds = new int[NB_HOLE];
+        redSeeds = new int[NB_HOLE];
 
-        for (int i = 1; i <=12;i++){
+        for (int i = 0; i <12;i++){
             blackSeeds[i] = 3;
             redSeeds[i] = 3;
         }
 
         init();
+        nextRequest();
 
     }
 
+    /**
+     * Permet d'initialiser la game
+     * @return
+     */
     public boolean init(){
         System.out.printf("Initialisation de la partie...\n");
         System.out.printf("Quel est le joueur qui commence en premier ? [robot|player]\n");
@@ -43,6 +51,32 @@ public class Game {
             return  init();
         }
     }
+
+    /**
+     * Permet d'avoir le coups suivant que le joueur souhaite
+     * @return
+     */
+    private Request nextRequest(){
+
+        String res = "";
+        Request request = new Request();
+        while (!(request.play > range) && (request.play < 12 - range)) {
+
+            res = "";
+
+            while (!res.matches("[0-9]*[a-zA-Z][0-9]*")) {
+                System.out.printf("Taper le coups à jouer:\n");
+                Scanner in = new Scanner(System.in);
+                res = in.nextLine();
+            }
+
+            request = new Request(res);
+
+        }
+        return request;
+    }
+
+
 
 }
 
